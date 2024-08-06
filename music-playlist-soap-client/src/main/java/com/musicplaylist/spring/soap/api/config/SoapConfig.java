93cd7b50399bf5ca35a6518e705b0245bcfc5abb
ivona-mircheska <92101084+ivona-mircheska@users.noreply.github.com>
2024-08-06 19:56:46 +0200

    Add files via upload

---
+++ music-playlist-soap-client/src/main/java/com/musicplaylist/spring/soap/api/config/SoapConfig.java
@@ -0,0 +1,17 @@
+package com.musicplaylist.spring.soap.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class SoapConfig {
+	
+	@Bean
+	public Jaxb2Marshaller marshaller() {
+		Jaxb2Marshaller marshaller=new Jaxb2Marshaller();
+		marshaller.setPackagesToScan("com.example.playlist.logging");
+		return marshaller;
+	}
+
+}
