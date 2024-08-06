f51404973b1606859ec5f303330a31454dc0b1a3
ivona-mircheska <92101084+ivona-mircheska@users.noreply.github.com>
2024-08-06 11:42:30 +0200

    Add files via upload

---
+++ music-playlist/src/main/java/com/musicplaylist/spring/soap/api/SpringBootSoap.java
@@ -0,0 +1,13 @@
+package com.musicplaylist.spring.soap.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoap {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoap.class, args);
+	}
+
+}
