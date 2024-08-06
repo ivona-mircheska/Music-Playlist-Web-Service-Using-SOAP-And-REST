bab702cdf34a3ec3cf3f2cae2d2aa1b77f636dba
ivona-mircheska <92101084+ivona-mircheska@users.noreply.github.com>
2024-08-06 19:28:38 +0200

    Add files via upload

---
+++ musicplaylist-rest/src/main/java/com/musicplaylist/musicplaylisttrest/MusicplaylistRestApplication.java
@@ -0,0 +1,13 @@
+package com.musicplaylist.musicplaylistrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicplaylistRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicplaylistRestApplication.class, args);
+	}
+
+}
