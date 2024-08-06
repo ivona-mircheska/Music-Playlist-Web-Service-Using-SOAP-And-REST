6eddaaf6748b576f94e252f285f09bccded6c9d6
ivona-mircheska <92101084+ivona-mircheska@users.noreply.github.com>
2024-08-06 19:34:13 +0200

    Add files via upload

---
+++ musicplaylist-rest/src/test/java/com/musicplaylist/musicplaylistrest/MusicplaylistRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicplaylist.musicplaylistrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicplaylistRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
